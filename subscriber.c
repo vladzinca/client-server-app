5398cef9207558baeeb83e3011d30480180b03ca
vladzinca <vlad_zinca@yahoo.com>
2022-05-09 19:41:44 +0300

    Implements subscribe, unsubscribe and exit functionalities for subscriber and also printing messages received from the server

--- subscriber.c
+++ subscriber.c
@@ -70,14 +70,46 @@ int main(int argc, char *argv[])
 
 			// trimite mesaje la server pe baza comenzii
 			if (strncmp(buffer, "subscribe", 9) == 0) {
+				Message msg;
+				memset(&msg, 0, sizeof(Message));
+				msg.command = 0;
+
+				const char s[1] = " ";
+				char* t = strtok(buffer, s);
+				t = strtok(NULL, s);
+				strcpy(msg.topic, t);
+				t = strtok(NULL, s);
+				int sf = t[0] - '0';
+
+				if (sf != 0 && sf != 1)
+					sf = 0;
+				msg.sf = sf;
+
+				printf("Subscribed to topic.\n");
+
+				n = send(tcpsockfd, &msg, sizeof(Message), 0);
 
 			} else if (strncmp(buffer, "unsubscribe", 11) == 0) {
+				Message msg;
+				memset(&msg, 0, sizeof(Message));
+				msg.command = 1;
+
+				const char s[1] = " ";
+				char* t = strtok(buffer, s);
+				t = strtok(NULL, s);
+				strcpy(msg.topic, t);
+
+				printf("Unsubscribed from topic.\n");
+
+				n = send(tcpsockfd, &msg, sizeof(Message), 0);
 
 			} else if (strncmp(buffer, "exit", 4) == 0) {
 				Message msg;
 				memset(&msg, 0, sizeof(Message));
 				msg.command = 2;
+
 				n = send(tcpsockfd, &msg, sizeof(Message), 0);
+
 				break;
 			}
 		}
@@ -89,7 +121,13 @@ int main(int argc, char *argv[])
 			n = recv(tcpsockfd, buffer, BUFLEN, 0);
 			DIE(n < 0, "recv");
 
-			// printf("Msg: %s\n", buffer);
+			if (n > 0) {
+				Message* msg = (Message*)buffer;
+				printf("%d:%u - %s - %s - %s\n", msg->ip, msg->port, msg->topic, msg->tip_date, msg->valoare_mesaj);
+			}
+			else
+				break;
+
 		}
 	}
 
