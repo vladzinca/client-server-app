6a1c4cb4dee80602605557ce3ae84dfa9f189817
vladzinca <vlad_zinca@yahoo.com>
2022-05-10 23:54:02 +0300

    Final push

--- helpers.h
+++ helpers.h
@@ -11,17 +11,19 @@
  *     DIE(fd == -1, "open failed");
  */
 
-#define DIE(assertion, call_description)	\
-	do {									\
-		if (assertion) {					\
-			fprintf(stderr, "(%s, %d): ",	\
-					__FILE__, __LINE__);	\
-			perror(call_description);		\
-			exit(EXIT_FAILURE);				\
-		}									\
-	} while(0)
+#define DIE(assertion, call_description)  \
+	do                                    \
+	{                                     \
+		if (assertion)                    \
+		{                                 \
+			fprintf(stderr, "(%s, %d): ", \
+					__FILE__, __LINE__);  \
+			perror(call_description);     \
+			exit(EXIT_FAILURE);           \
+		}                                 \
+	} while (0)
 
-#define BUFLEN		256	// dimensiunea maxima a calupului de date
-#define MAX_CLIENTS	5	// numarul maxim de clienti in asteptare
+#define BUFLEN 256	  // dimensiunea maxima a calupului de date
+#define MAX_CLIENTS 5 // numarul maxim de clienti in asteptare
 
 #endif
